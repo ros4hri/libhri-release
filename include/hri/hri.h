167095eabc48056acbb9f9dccf3c9addb21f1e91
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-04-27 16:58:26 +0200

    Rebase from 'debian/noetic/hri'

--- include/hri/hri.h
+++ include/hri/hri.h
@@ -74,15 +74,19 @@ public:
 
   ~HRIListener();
 
-  /** \brief Provided callback is called every time a new person is detected.
-   */
-  // void subscribe(std::function<void(const Person&)>& callback);
-
   /** \brief Returns the list of currently detected faces, mapped to their IDs
    *
    * Faces are returned as constant std::weak_ptr as they may disappear at any point.
    */
   std::map<ID, FaceWeakConstPtr> getFaces() const;
+
+  /** \brief Registers a callback function, to be invoked everytime a new face
+   * is detected.
+   */
+  void onFace(std::function<void(FaceWeakConstPtr)> callback)
+  {
+    face_callbacks.push_back(callback);
+  }
 
   /** \brief Returns the list of currently detected bodies, mapped to their IDs
    *
@@ -90,15 +94,41 @@ public:
    */
   std::map<ID, BodyWeakConstPtr> getBodies() const;
 
+  /** \brief Registers a callback function, to be invoked everytime a new body
+   * is detected.
+   */
+  void onBody(std::function<void(BodyWeakConstPtr)> callback)
+  {
+    body_callbacks.push_back(callback);
+  }
+
   /** \brief Returns the list of currently detected voices, mapped to their IDs
    *
    * Voices are returned as constant std::weak_ptr as they may disappear at any point.
    */
   std::map<ID, VoiceWeakConstPtr> getVoices() const;
 
+  /** \brief Registers a callback function, to be invoked everytime a new voice
+   * is detected.
+   */
+  void onVoice(std::function<void(VoiceWeakConstPtr)> callback)
+  {
+    voice_callbacks.push_back(callback);
+  }
+
+
   /** \brief Returns the list of currently detected persons, mapped to their IDs
    */
   std::map<ID, PersonConstPtr> getPersons() const;
+
+  /** \brief Registers a callback function, to be invoked everytime a new person
+   * is detected.
+   */
+  void onPerson(std::function<void(PersonConstPtr)> callback)
+  {
+    person_callbacks.push_back(callback);
+  }
+
 
 
 private:
@@ -111,9 +141,16 @@ private:
   std::map<FeatureType, ros::Subscriber> feature_subscribers_;
 
   std::map<ID, FaceConstPtr> faces;
+  std::vector<std::function<void(FaceWeakConstPtr)>> face_callbacks;
+
   std::map<ID, BodyConstPtr> bodies;
+  std::vector<std::function<void(BodyWeakConstPtr)>> body_callbacks;
+
   std::map<ID, VoiceConstPtr> voices;
+  std::vector<std::function<void(VoiceWeakConstPtr)>> voice_callbacks;
+
   std::map<ID, PersonConstPtr> persons;
+  std::vector<std::function<void(PersonConstPtr)>> person_callbacks;
 };
 
 }  // namespace hri
