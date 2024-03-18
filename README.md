0e03eda3e2c02576d8bc9e57cb082c725730e88d
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2024-03-18 14:06:39 +0100

    Imported upstream version '2.3.0' of 'upstream'

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-libhri
-======
+# ROS4HRI helper library
 
-`libhri` is a wrapper library for the ROS4HRI framework, significantly easing
-accessing the robot's perceptions of surrounding humans.
+A wrapper library for the [ROS4HRI](https://wiki.ros.org/hri) framework,
+significantly easing accessing the robot's perceptions of surrounding humans.
 
+It contains two packages:
+- `hri`: the C++ implementation package
+- `pyhri`: the Python implementation package, wrapping the C++ one with Pybind11
+
+**Note: this branch only contains ROS 2 support. For ROS 1, check the `main` branch.**
+**Note: this repository only contains `pyhri` ROS 2 support. For ROS 1, check the `pyhri` repository.**
+
+## Documentation
+
+For the `pyhri` documentation, first `import hri`, then use `help(hri.<class>)`.
+
+The main entry point for using the library is the `HRIListener` class.
+
+## Example
+
+For an example of usage, you can check the example:
+- C++: `hri/src/example.cpp` _or_
+- Python: `pyhri/pyhri/example.py`
+
+They are installed by default and can be tested by executing:
+- In a separate terminal:
+  1. `apt install ros-humble-usb-cam`
+  2. `ros2 run usb_cam usb_cam_node_exe`
+- In a separate terminal:
+  1. `apt install ros-humble-hri-face-detect`
+  2. `ros2 launch hri_face_detect face_detect.launch.py`
+- In a separate terminal, either:
+  - C++: `ros2 run hri hri_example`  _or_
+  - Python: `ros2 run pyhri pyhri_example`
+
+Note that the Python implementation actually spawns two nodes.
---
+++ hri/src/example.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <functional>
+#include <memory>
+
+#include "hri/hri.hpp"
+#include "opencv2/opencv.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+class ShowFaces : public rclcpp::Node
+{
+public:
+  ShowFaces()
+  : rclcpp::Node("hri_example")
+  {}
+
+  void init()
+  {
+    // "shared_from_this()" cannot be used in the constructor!
+    hri_listener_ = hri::HRIListener::create(shared_from_this());
+    timer_ = create_wall_timer(500ms, std::bind(&ShowFaces::timer_callback, this));
+  }
+
+  void timer_callback()
+  {
+    auto faces = hri_listener_->getFaces();
+    for (auto const & [face_id, face] : faces) {
+      if (auto cropped = face->cropped()) {
+        cv::imshow("Cropped face " + face_id, *cropped);
+      }
+      if (auto aligned = face->aligned()) {
+        cv::imshow("Aligned face " + face_id, *aligned);
+      }
+      cv::waitKey(10);
+    }
+  }
+
+private:
+  std::shared_ptr<hri::HRIListener> hri_listener_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ShowFaces>();
+  node->init();
+  rclcpp::spin(node->get_node_base_interface());
+  rclcpp::shutdown();
+  return 0;
+}
